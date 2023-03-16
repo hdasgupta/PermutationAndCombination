7eb0cdaff96c2d64db730deb8af4acdff82d3304
hdasgupta <himaghna.dasgupta@gmail.com>
2023-03-17 02:07:51 +0530

    Test Cases Update

--- src/test/kotlin/org/combination/tests/PermutationTests.kt
+++ src/test/kotlin/org/combination/tests/PermutationTests.kt
@@ -1,10 +1,13 @@
 package org.combination.tests
 
 import org.combination.permutaion.Permutation
+import org.combination.permutaion.Result
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
+import kotlin.math.min
 import kotlin.test.assertContentEquals
 
 @SpringBootTest
@@ -19,13 +22,31 @@ class PermutationTests {
     fun permutation() {
         val lists = permutation.permutation(words)
 
-        assertContentEquals(lists, listOf(
+        print(lists)
+
+        assertContentEquals(lists.toList().map { it.toList() }, listOf(
+            listOf("Blue","Green","Red"),listOf("Blue","Red","Green"),listOf("Green","Blue","Red"),listOf("Green","Red","Blue"),listOf("Red","Blue","Green"),
             listOf("Red","Green","Blue"),
-            listOf("Red","Blue","Green"),
-            listOf("Blue","Red","Green"),
-            listOf("Green","Red","Blue"),
-            listOf("Green","Blue","Red"),
-            listOf("Blue","Green","Red"),
         ))
     }
+
+    fun print(lists: TreeSet<Result>, lineCount:Int=5) =
+        lists.toList().let {
+                list->
+            (list.indices step lineCount).forEach {
+                (it until min(it+lineCount, list.size)).forEach {
+                        i->
+                    val l = list[i]
+                    print("listOf(")
+                    l.forEach {
+                            s->
+                        print("\"$s\",")
+                    }
+                    print("\b),")
+                }
+                println()
+            }
+        }
+
+
 }
